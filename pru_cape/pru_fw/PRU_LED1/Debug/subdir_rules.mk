f806b57f9194bb2c3e1a0235e9d770e71f413f49
pelengws_0126 <skdvoip@gmail.com>
2016-02-08 17:15:28 +0300

    Первый День Работы модифицировал рабочее ПО

--- pru_cape/pru_fw/PRU_LED1/Debug/subdir_rules.mk
+++ pru_cape/pru_fw/PRU_LED1/Debug/subdir_rules.mk
@@ -6,7 +6,7 @@
 PRU_LED1.obj: ../PRU_LED1.c $(GEN_OPTS) $(GEN_HDRS)
 	@echo 'Building file: $<'
 	@echo 'Invoking: PRU Compiler'
-	"C:/ti/ccsv6/tools/compiler/ti-cgt-pru_2.1.1/bin/clpru" -v3 --include_path="C:/ti/ccsv6/tools/compiler/ti-cgt-pru_2.1.1/include" --include_path="../../../../include" -g --define=am3359 --define=pru0 --diag_warning=225 --display_error_number --diag_wrap=off --endian=little --hardware_mac=on --preproc_with_compile --preproc_dependency="PRU_LED1.pp" $(GEN_OPTS__FLAG) "$<"
+	"C:/ti/ccsv6/tools/compiler/ti-cgt-pru_2.1.2/bin/clpru" -v3 --include_path="C:/ti/ccsv6/tools/compiler/ti-cgt-pru_2.1.2/include" --include_path="../../../../include" -g --define=am3359 --define=pru0 --diag_wrap=off --diag_warning=225 --display_error_number --endian=little --hardware_mac=on --preproc_with_compile --preproc_dependency="PRU_LED1.pp" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
